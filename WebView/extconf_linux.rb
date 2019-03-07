dacade37625b09c2f0db2ff3405679669d68c9af
Arun <arun@siara.cc>
2019-03-07 23:59:25 +0530

    Fix linux

--- WebView/extconf_linux.rb
+++ WebView/extconf_linux.rb
@@ -1,8 +1,8 @@
 # Loads mkmf which is used to make makefiles for Ruby extensions
 require 'mkmf'
 
-$CFLAGS = '-DWEBVIEW_COCOA=1 -DWEBVIEW_STATIC=1'
-$LDFLAGS = '-framework WebKit'
+$CFLAGS = '-DWEBVIEW_GTK=1 `pkg-config --cflags gtk+-3.0 webkit2gtk-4.0`'
+$LDFLAGS = '`pkg-config --libs gtk+-3.0 webkit2gtk-4.0`'
 
 # Give it a name
 extension_name = 'WebView'
